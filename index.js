008c45cb67e3635d0cb110433517322a37a2eb67
Tahmeed Nabi <tahmeed.nabi@mudbath.com.au>
2024-11-02 01:42:01 +1100

    Refactor code

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
-// expecting time to be a string in the format like '8:15' or '12:30'
-const numberToWordMap = {
-  0: 'twelve',
-  1: 'one',
-  2: 'two',
-  3: 'three',
-  4: 'four',
-  5: 'five',
-  6: 'six',
-  7: 'seven',
-  8: 'eight',
-  9: 'nine',
-  10: 'ten',
-  11: 'eleven',
-  12: 'twelve',
-  13: 'thirteen',
-  14: 'fourteen',
-  15: 'quarter',
-  16: 'sixteen',
-  17: 'seventeen',
-  18: 'eighteen',
-  19: 'nineteen',
-  20: 'twenty',
-  21: 'twenty one',
-  22: 'twenty two',
-  23: 'twenty three',
-  24: 'twenty four',
-  25: 'twenty five',
-  26: 'twenty six',
-  27: 'twenty seven',
-  28: 'twenty eight',
-  29: 'twenty nine',
-};
+const { numberToWordMap } = require('./num-to-words-map');
 
+// expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
   const split = time.split(':');
   const moduloHour = Number(split[0] % 12);
